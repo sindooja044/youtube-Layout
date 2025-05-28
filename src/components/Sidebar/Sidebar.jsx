53f478a41ffcb48a1142d55fbbaa8299c7b78226
sindooja044 <aaeer015@gmail.com>
2025-05-28 12:13:39 +0530

    changed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
+import home from "../../assets/home.svg";
+import explore from "../../assets/explore.svg";
+import subscriptions from "../../assets/subscriptions.svg";
+import originals from "../../assets/originals.svg";
+import youtubeMusic from "../../assets/youtube-music.svg";
+import library from "../../assets/library.svg";
+
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-      hello
+    <div className="sidebar">
+      <div className="home">
+        <img src={home} alt="" />
+        <p>Home</p>
+      </div>
+      <div className="home">
+        <img src={explore} alt="" />
+        <p> explore</p>
+      </div>
+      <div className="home">
+        <img src={subscriptions} alt="" />
+        <p> subscriptions1</p>
+      </div>
+      <div className="home">
+        <img src={originals} alt="" />
+        <p> originals1</p>
+      </div>
+      <div className="home">
+        <img src={youtubeMusic} alt="" />
+        <p> music</p>{" "}
+      </div>
+      <div className="home">
+        <img src={library} alt="" />
+        <p> library</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
